6376d14ff651bd4337fe39d461a903df291e0153
U-FR01_AWL\a532881 <a532881@L82283.fr01.awl.atosorigin.net>
2014-08-27 14:11:23 +0200

    Add Base64 encoded part support

--- app/scripts/routes/messageRoute.js
+++ app/scripts/routes/messageRoute.js
@@ -12,16 +12,15 @@ Client.MessageRoute = Ember.Route.extend({
 			type: 'GET',
 			dataType: 'json'
 		}).then(function (result) {
-			//Extract the first message result
+            //Extract the first message result
 			if (result.length < 1) {
 				Ember.Logger.warn('The server returns no result for message id#' + param.id + ' in box#' + box.name);
 				return null;
 			}
-			return result[0];
-		}).then(function (message) {
+            
 			//Download the parts displayed
+			var message = result[0];
 			var neededParts = self.getNeededParts.call(self, message.attrs.struct);
-			Ember.Logger.debug('Needed parts: ' + JSON.stringify(neededParts));
 			return Ember.RSVP.all(neededParts).then(function (results) {
 				Ember.Logger.debug('All needed part has been received (' + results.length + ')');
 				return message;
@@ -31,11 +30,10 @@ Client.MessageRoute = Ember.Route.extend({
 
 	afterModel: function (model) {
 		if (model === null) {
-			Ember.Logger.debug('Model is null: Transition to messages');
+			Ember.Logger.debug('Model is null: Transition to messages route');
 			this.transitionTo('messages');
 		}
 	},
-
 
 	getNeededParts: function getNeededParts(part) {
 		if (part instanceof Array) {
@@ -71,8 +69,11 @@ Client.MessageRoute = Ember.Route.extend({
 		var box = this.modelFor('box');
 		var id = this.get('id');
 
+        //Ask the server for the part content
 		var self = this;
 		part.content = null;
+        part.decodedContent = null;
+        Ember.Logger.info('Ask the server for part#' + part.partID + ' message#' + id + ' in box#' + box.name);
 		return Ember.$.ajax({
 			url: Client.REST_SERVER + '/boxes/' + box.name + '/messages?bodies=' + part.partID + '&ids=' + id,
 			type: 'GET',
@@ -86,25 +87,30 @@ Client.MessageRoute = Ember.Route.extend({
 				Ember.Logger.warn('No bodies part for part#' + part.partID + ' message#' + id + ' in box#' + box.name);
 				return null;
 			}
+            
+            //Store the part in the model object
 			part.content = result[0].bodies[part.partID];
 			part.decodedContent = self.decodePartContent(part);
-
 			Ember.Logger.debug('Part#' + part.partID + ' message#' + id + ' in box#' + box.name + ' received. Length: ' + part.content.length);
 			return part.content;
 		});
 	},
 
 	decodePartContent: function (part) {
-		var result;
-		//TODO: make an object with methods rather than this ugly pile of functions ;)
-		if (part.encoding === 'quoted-printable') {
-			result = quotedPrintable.decode(part.content);
-		}
+		var result = part.content;
 
-		if (part.params.charset === "UTF-8") {
-			//TODO: Handler other charset
-			result = decodeURIComponent(escape(result));
-		}
+        //Decode specials MIME encoding
+        if(part.encoding) {
+            var encoding = part.encoding.toLowerCase();
+            if (encoding === 'quoted-printable') {
+                result = quotedPrintable.decode(part.content);
+            } elseif(encoding === 'base64') {
+                result = window.atob(part.content);
+            }
+        }
+
+        //Clear special characters
+        result = decodeURIComponent(escape(result));
 
 		return result;
 	}
