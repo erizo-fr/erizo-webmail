21e0fbac761cfce69f12c451a8e0fd1469d00b6e
Timothée EID <timothee.eid@erizo.fr>
2015-03-14 22:04:22 +0100

    Update date formatter

--- app/scripts/helpers/dateFormatter.js
+++ app/scripts/helpers/dateFormatter.js
@@ -1,17 +1,30 @@
-Ember.Handlebars.helper('date-formatter', function(value, input, output) {
-	var date;
-	if(input) {
-		date = moment(value, input);
-	} else {
-		date = moment(value);
-	}
-	if(date.isValid() !== true) {
-		return 'Invalid date';
-	}
-	
-	if(output) {
-		return date.format(output);
-	} else {
-		return date.format('L');
-	}
+Ember.Handlebars.helper('date-formatter', function (value, input, output) {
+    var date;
+    if (input) {
+        date = moment(value, input);
+    } else {
+        date = moment(value);
+    }
+    if (! date.isValid()) {
+        return 'Invalid date';
+    }
+
+    
+    var now = moment();
+    if(! output) {
+        if (date.isSame(lastDay, 'day')) {
+            output = 'LT'; //8:30 PM
+        } else {
+            output = 'll' //Sep 4 1986
+        }
+    } else if (output === 'message-list') {
+        var lastDay = moment().subtract(1, 'day');
+        if (date.isSame(lastDay, 'day') || date.isSame(now, 'day')) {
+            output = 'LT'; //8:30 PM
+        } else {
+            output = 'll'; //Sep 4 1986
+        }
+    }
+    
+    return date.format(output);
 });
