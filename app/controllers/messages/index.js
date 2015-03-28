89aec2f92daf657830b7481010e0f31c4fce0665
Timothée EID <timothee.eid@erizo.fr>
2015-03-29 00:29:23 +0100

    Fix wrong index issue

--- app/controllers/messages/index.js
+++ app/controllers/messages/index.js
@@ -5,35 +5,35 @@ export default Ember.ObjectController.extend({
 	needs: ['messages', 'box'],
 
 	messageCategories: [],
-	
+
 	currentPage: -1,
 	pageSize: 10,
 	isMessagesLoading: false,
 	hasMorePages: false,
-    
-    init: function() {
+
+	init: function () {
 		this._super.apply(this, arguments);
-		
-        var windowHeight = $(window).height();
-        Ember.Logger.debug('Windows height: ' + windowHeight);
-        var pageSize = Math.floor((windowHeight - 150) / 35);
-        Ember.Logger.debug('pageSize: ' + pageSize);
-        this.set('pageSize', pageSize);
-    },
+
+		var windowHeight = $(window).height();
+		Ember.Logger.debug('Windows height: ' + windowHeight);
+		var pageSize = Math.floor((windowHeight - 150) / 35);
+		Ember.Logger.debug('pageSize: ' + pageSize);
+		this.set('pageSize', pageSize);
+	},
 
 	actions: {
-		loadMoreMessages: function() {
+		loadMoreMessages: function () {
 			this.loadMoreMessages();
 		},
-		deleteSelectedMessages: function() {
+		deleteSelectedMessages: function () {
 			Ember.Logger.debug('Action received: Delete selected messages');
 
 			Ember.Logger.warn('TODO');
 		}
 	},
 
-	loadMoreMessages: function() {
-		if(this.get('isMessagesLoading')) {
+	loadMoreMessages: function () {
+		if (this.get('isMessagesLoading')) {
 			return;
 		} else {
 			this.set('isMessagesLoading', true);
@@ -46,9 +46,9 @@ export default Ember.ObjectController.extend({
 		var currentPage = this.get('currentPage');
 		var nextPage = currentPage + 1;
 		var totalElements = box.messages.total;
-		var lastPage = Math.ceil(totalElements / pageSize) -1;
+		var lastPage = Math.ceil(totalElements / pageSize) - 1;
 
-		if(currentPage >= lastPage) {
+		if (currentPage >= lastPage) {
 			Ember.Logger.info('Try to load more message (page#' + nextPage + ') but no more pages are available (lastPage: ' + lastPage + ')');
 			this.set('hasMorePages', false);
 			this.set('isMessagesLoading', false);
@@ -58,7 +58,7 @@ export default Ember.ObjectController.extend({
 		//Load the initial messages
 		Ember.Logger.debug('Getting the message page#' + nextPage + '[' + pageSize + '] of box#' + box.path);
 		var idMin = Math.max(0, nextPage * pageSize);
-		var idMax = Math.min(totalElements - 1, idMin + pageSize);
+		var idMax = Math.min(totalElements - 1, idMin + pageSize) + 1;
 		var self = this;
 		var ids = messagesOrder.slice(idMin, idMax);
 		Api.getMessagesByIds(box.path, ids).then(function (newMessages) {
@@ -67,8 +67,8 @@ export default Ember.ObjectController.extend({
 
 				var getCategory = function (categoryKey) {
 					var i;
-					for(i = 0; i < messageCategories.length; i++) {
-						if(messageCategories[i].key === categoryKey) {
+					for (i = 0; i < messageCategories.length; i++) {
+						if (messageCategories[i].key === categoryKey) {
 							return messageCategories[i];
 						}
 					}
@@ -76,19 +76,19 @@ export default Ember.ObjectController.extend({
 					//Create category
 					var category = {
 						key: categoryKey,
-						messages : []
+						messages: []
 					};
-					if(categoryKey === 'TODAY') {
+					if (categoryKey === 'TODAY') {
 						category.label = 'Today';
-					} else if(categoryKey === 'LAST_DAY') {
+					} else if (categoryKey === 'LAST_DAY') {
 						category.label = 'Yesterday';
-					} else if(categoryKey === 'WEEK') {
+					} else if (categoryKey === 'WEEK') {
 						category.label = 'This week';
-					} else if(categoryKey === 'LAST_WEEK') {
+					} else if (categoryKey === 'LAST_WEEK') {
 						category.label = 'Last week';
-					} else if(categoryKey === 'MONTH') {
+					} else if (categoryKey === 'MONTH') {
 						category.label = 'This month';
-					} else if(categoryKey === 'LAST_MONTH') {
+					} else if (categoryKey === 'LAST_MONTH') {
 						category.label = 'Last month';
 					} else {
 						category.label = categoryKey;
@@ -105,24 +105,24 @@ export default Ember.ObjectController.extend({
 					var lastWeek = moment().subtract(7, 'day');
 					var lastMonth = moment().subtract(1, 'month');
 
-					if(messageDate.isSame(now, 'day')) {
-					   return 'TODAY';
-				   	} else if(messageDate.isSame(lastDay, 'day')) {
-					   return 'LAST_DAY';
-					} else if(messageDate.isSame(now, 'week')) {
-					   return 'WEEK';
-					} else if(messageDate.isSame(lastWeek, 'week')) {
-					   return 'LAST_WEEK';
-					} else if(messageDate.isSame(now, 'month')) {
-					   return 'MONTH';
-				   	} else if(messageDate.isSame(lastMonth, 'month')) {
-					   return 'LAST_MONTH';
-				   	} else {
+					if (messageDate.isSame(now, 'day')) {
+						return 'TODAY';
+					} else if (messageDate.isSame(lastDay, 'day')) {
+						return 'LAST_DAY';
+					} else if (messageDate.isSame(now, 'week')) {
+						return 'WEEK';
+					} else if (messageDate.isSame(lastWeek, 'week')) {
+						return 'LAST_WEEK';
+					} else if (messageDate.isSame(now, 'month')) {
+						return 'MONTH';
+					} else if (messageDate.isSame(lastMonth, 'month')) {
+						return 'LAST_MONTH';
+					} else {
 						return messageDate.format('MMM YYYY');
 					}
 				};
 
-				var insertMessage = function(message) {
+				var insertMessage = function (message) {
 					var messageDateCategory = getMessageDateCategory(message);
 					var messageCategory = getCategory(messageDateCategory);
 					messageCategory.messages.pushObject(message);
@@ -130,7 +130,7 @@ export default Ember.ObjectController.extend({
 
 				//Insert the messages
 				var newMessagesReversed = newMessages.reverse();
-				Ember.$.each(newMessagesReversed, function( index, value ) {
+				Ember.$.each(newMessagesReversed, function (index, value) {
 					insertMessage(value);
 				});
 
