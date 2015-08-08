bb29b9fa068ec681d13cd2b6816f8de23c382d0e
Timothée EID <timothee.eid@erizo.fr>
2015-08-08 13:18:39 +0200

    Update linters

--- server/proxies/api.js
+++ server/proxies/api.js
@@ -1,17 +1,18 @@
-var proxyPath = '/api';
+var proxyPath = "/api"
 
-module.exports = function(app) {
-  // For options, see:
-  // https://github.com/nodejitsu/node-http-proxy
-  var proxy = require('http-proxy').createProxyServer({});
+module.exports = function (app) {
+	// For options, see:
+	// https://github.com/nodejitsu/node-http-proxy
+	var proxy = require("http-proxy").createProxyServer({})
 
-  proxy.on('error', function(err, req) {
-    console.error(err, req.url);
-  });
+	proxy.on("error", function (err, req) {
+		console.error(err, req.url)
+	})
 
-  app.use(proxyPath, function(req, res, next){
-    // include root path in proxied request
-    req.url = proxyPath + '/' + req.url;
-    proxy.web(req, res, { target: 'http://localhost:8081/' });
-  });
-};
+	app.use(proxyPath, function (req, res) {
+		// include root path in proxied request
+		req.url = proxyPath + "/" + req.url
+		proxy.web(req, res, {target: "http://localhost:8081/"})
+	})
+	console.info("Proxy server is bound to " + proxyPath)
+}
